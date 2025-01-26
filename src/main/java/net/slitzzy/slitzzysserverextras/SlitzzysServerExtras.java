e9ed723e6bc0e4215fb0d4e35a858479c1a5e21a
SLITZZY <Slitzzy.business@gmail.com>
2025-01-26 10:18:58 -0500

    Initial commit

---
+++ src/main/java/net/slitzzy/slitzzysserverextras/SlitzzysServerExtras.java
@@ -0,0 +1,17 @@
+package net.slitzzy.slitzzysserverextras;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SlitzzysServerExtras implements ModInitializer {
+
+	public static final String MOD_ID = "slitzzys-server-extras";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/slitzzy/slitzzysserverextras/SlitzzysServerExtrasClient.java
@@ -0,0 +1,10 @@
+package net.slitzzy.slitzzysserverextras;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SlitzzysServerExtrasClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
